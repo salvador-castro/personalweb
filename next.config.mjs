696849f11eafe60f117295e1dc0184cb815c7b37
Salvador Castro <salvacastro06@gmail.com>
2025-11-24 22:38:27 -0300

    Remove export output and ignore eslint on build

--- next.config.mjs
+++ next.config.mjs
@@ -1,62 +1,64 @@
-import mdx from "@next/mdx";
+import mdx from '@next/mdx'
 
 const withMDX = mdx({
   extension: /\.mdx?$/,
-  options: {},
-});
+  options: {}
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // 👈 esto activa el static export en next build
-
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-  transpilePackages: ["next-mdx-remote"],
+  /*   output: 'export', // 👈 esto activa el static export en next build*/
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  transpilePackages: ['next-mdx-remote'],
   sassOptions: {
-    compiler: "modern",
-    silenceDeprecations: ["legacy-js-api"],
+    compiler: 'modern',
+    silenceDeprecations: ['legacy-js-api']
   },
   images: {
     // mantenés tus remotePatterns
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.simpleicons.org",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'cdn.simpleicons.org',
+        pathname: '/**'
       },
       {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        pathname: "/devicons/devicon/**",
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/devicons/devicon/**'
       },
       {
-        protocol: "https",
-        hostname: "cdn.jsdelivr.net",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        pathname: '/**'
       },
       {
-        protocol: "https",
-        hostname: "vitejs.dev",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'vitejs.dev',
+        pathname: '/**'
       },
       {
-        protocol: "https",
-        hostname: "cdn.worldvectorlogo.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'cdn.worldvectorlogo.com',
+        pathname: '/**'
       },
       {
-        protocol: "https",
-        hostname: "img.icons8.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+        pathname: '/**'
       },
       {
-        protocol: "https",
-        hostname: "user-images.githubusercontent.com",
-        pathname: "/**",
-      },
+        protocol: 'https',
+        hostname: 'user-images.githubusercontent.com',
+        pathname: '/**'
+      }
     ],
     // y esto evita problemas con next/image en export estático
-    unoptimized: true,
-  },
-};
+    unoptimized: true
+  }
+}
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
